7546a38b274516a1fc713f159a7783a7ed9e4d34
UNIDY2002 <UNIDY2002@outlook.com>
2020-09-28 01:24:00 +0800

    Basic framework.

---
+++ lopt.user.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+// ==UserScript==
+// @name        LoPT
+// @namespace   https://github.com/UNIDY2002/LoPT
+// @description Lab. of Physics Tool
+// @author      UNIDY2002
+// @version     1.0.0
+// @match       *://166.111.214.78/Select_Select.php?weekselect=*
+// @require     https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
+// ==/UserScript==
+
+$(document).ready(function () {
+    $("input[type=RADIO]").click(function () {
+        alert("Clicked!");
+    });
+});
